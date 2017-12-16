061fa4035706963108afd432c53b2417b24ec492
gabrielBlankenburg <gabriel.blankenburg.p.silva@gmail.com>
2017-12-16 02:08:05 -0200

    Fixed some bugs

--- dictionary_attack.py
+++ dictionary_attack.py
@@ -37,17 +37,19 @@ def main(argv):
 	arg_file_hashes = ""
 	arg_dictionary_file = ""
 	arg_output_file = ""
+	arg_hash = ""
+	arg_dictionary = ""
 	# Check the arguments
 	try:
 		opts, args = getopt.getopt(argv,"h",["fhashes=","fdic=","fout="])
 	except getopt.GetoptError:
-		print 'test.py --fhashes <file_with_hashes> --fdic <dictionary_file> --fout <output_file>'
+		print 'dictionary_attack.py --fhashes <file_with_hashes> --fdic <dictionary_file> --fout <output_file>'
 		sys.exit()
 
 	# Go through the options
 	for opt, arg in opts:
 		if opt == '-h':
-			'test.py --fhashes <file_with_hashes> --fdic <dictionary_file> --fout <output_file>'
+			'dictionary_attack.py --fhashes <file_with_hashes> --fdic <dictionary_file> --fout <output_file>'
 			sys.exit()
 		elif opt in ("--fhashes"):
 			arg_file_hashes = arg
@@ -55,16 +57,34 @@ def main(argv):
 			arg_dictionary_file = arg
 		elif opt in ("--fout"):
 			arg_output_file = arg
+		elif opt in ("--hash"):
+			arg_hash = arg
+		elif opt in ("--dictionary"):
+			arg_dictionary = arg
 
-	file_hashes = open(arg_file_hashes, 'r')
-	hash_content = file_hashes.read()
-	hash_list = hash_content.splitlines()
-	file_hashes.close()
+	# The hashes from a file or command line
+	if arg_file_hashes != "":
+		file_hashes = open(arg_file_hashes, 'r')
+		hash_content = file_hashes.read()
+		hash_list = hash_content.splitlines()
+		file_hashes.close()
+	elif arg_hash != "":
+		hash_list = [arg_hash]
+	else:
+		print "You should insert a hash! type python dictionary_attack.py -h to help"
+		exit()
 
-	file_dictionary = open(arg_dictionary_file, 'r')
-	dictionary_content = file_dictionary.read()
-	dictionary_list = dictionary_content.splitlines()
-	file_dictionary.close()
+	# The dictionary
+	if arg_dictionary_file != "":
+		file_dictionary = open(arg_dictionary_file, 'r')
+		dictionary_content = file_dictionary.read()
+		dictionary_list = dictionary_content.splitlines()
+		file_dictionary.close()
+	elif arg_dictionary != "":
+		dictionary_list = list(arg_dictionary)
+	else:
+		print "You should insert a hash! type python dictionary_attack.py -h to help"
+		exit()
 
 
 	known = {}
